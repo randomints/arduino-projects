1a13810b5be2dd3298ca0007cc6da02243b789a4
randomints <raahimazfar@gmail.com>
2025-03-05 15:56:36 +0300

    Add files via upload
    document the code

--- cell.ino
+++ cell.ino
@@ -1,12 +1,13 @@
 #include  <Wire.h>
 #include  <LiquidCrystal_I2C.h>
 
+// the four lines of the LCD
 String line0 = "Click on something!";
 String line1;
 String line2;
 String line3;
 
-
+// variables for the parts of the cell
 int vacc = 11;
 int mito = 2;
 int chloro = 10;
@@ -14,59 +15,71 @@ int nuc = 9;
 int cyto = 12;
 int choose = 6;
 int knob = A0;
-int btn = 7;
+int wall = 7;
 
+// initialize the LCD
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup()
 {
   Serial.begin(9600);
-  
+
   pinMode(knob, INPUT);
 
-  pinMode(btn, INPUT);
-  digitalWrite(btn, HIGH);
+  // cell wall button initialization
+  pinMode(wall, INPUT);
+  digitalWrite(wall, HIGH);
 
+  // vaccuole button initialization
   pinMode(vacc, INPUT);
   digitalWrite(vacc, HIGH);
 
+  // mitochondria button initialization
   pinMode(mito, INPUT);
   digitalWrite(mito, HIGH);
 
+  // chloroplast button initialization
   pinMode(chloro, INPUT);
   digitalWrite(chloro, HIGH);
 
+  // nucleus button initialization
   pinMode(nuc, INPUT);
   digitalWrite(nuc, HIGH);
 
+  // cytoplasm button initialization
   pinMode(cyto, INPUT);
   digitalWrite(cyto, HIGH);
 
+  // selection button initialization
   pinMode(choose, INPUT);
   digitalWrite(choose, HIGH);
-  
 
+  // start the LCD
   lcd.init();
   lcd.backlight();
+
+  // start the serial monitor
   Serial.begin(9600);
 
+  // clear the LCD and print some test on to the LCD
   lcd.write("               ");
-
   lcd.setCursor(0, 0);
-
   lcd.print("Click on something!");
 }
 
 void loop()
 {
+  // get values of the buttons and the knob
   int mitoVal = digitalRead(mito);
   int vaccVal = digitalRead(vacc);
   int chloroVal = digitalRead(chloro);
   int nucVal = digitalRead(nuc);
   int cytoVal = digitalRead(cyto);
   int chooseVal = digitalRead(choose);
-  int btnVal = digitalRead(btn);
+  int wallVal = digitalRead(wall);
   int knobVal = analogRead(knob);
+
+  // print on the screen based off what button has been pressed
 
   if (vaccVal == 0) {
     line0 = "Name: Vacuole";
@@ -115,6 +128,7 @@ void loop()
 
   }
 
+  // selection menu for the cell membrane and cell wall
   if (chooseVal == 0) {
     line0 = "Choose:";
     line1 = "Cell Wall";
@@ -148,7 +162,7 @@ void loop()
       write();
     }
 
-    if (knobVal < 509 && btnVal == 0) {
+    if (knobVal < 509 && wallVal == 0) {
       line0 = "Name: Cell Membrane";
       line1 = "-a thin skin around";
       line2 = "the cell that lets";
@@ -156,17 +170,13 @@ void loop()
 
       write();
     }
-
     write();
   }
-
-
-  
   lcd.setCursor(0,0);
-
-  
 }
 
+// function to write to the LCD
+// all it does is print the line variables at lines 5 to 8. It also sets the cursor position for those lines
 void write() {
   lcd.clear();
   lcd.setCursor(0, 0);
